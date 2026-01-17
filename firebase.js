636082a72c33a5d16cf8c8977ac0dd4a7ad7ce5f
doctorianforde <doctorianforde@gmail.com>
2026-01-17 16:35:25 -0400

    moved files

--- firebase.js
+++ firebase.js
@@ -1,14 +1,16 @@
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyAcRgxzA0DvlsQUSjz_8SUR-wDKmCQYMlU",
+  authDomain: "my-chart-app-a6f67.firebaseapp.com",
+  projectId: "my-chart-app-a6f67",
+  storageBucket: "my-chart-app-a6f67.firebasestorage.app",
+  messagingSenderId: "657729241412",
+  appId: "1:657729241412:web:70b0ff7e18000ffe081711"
 };
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
